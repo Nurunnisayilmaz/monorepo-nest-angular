9d4ce505db431d6881633a4eef6a8da4ee48d33a
nisa <nurunnisa9@gmail.com>
2022-10-01 17:43:57 +0300

    file layout

--- apps/frontend/src/app/app-routing.module.ts
+++ apps/frontend/src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './todo-list/todo-list.component';
 
 const routes: Routes = [
-  { path: 'todos', component: TodoListComponent },
-  {path: '', redirectTo: 'todos', pathMatch: 'full'}
+  { path: 'app', component: TodoListComponent },
+  {path: '', redirectTo: 'app', pathMatch: 'full'}
 ];
 
 @NgModule({
